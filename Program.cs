6e48968c5badb6706f00cfb3b938fd0c8e2eda75
Saiam-Canedo <canedo.saiam@rpn.ch>
2017-11-10 11:11:05 +0100

    Add Insert Value For calculatrice

--- Program.cs
+++ Program.cs
@@ -10,6 +10,54 @@ namespace Calculatrice
     {
         static void Main(string[] args)
         {
+            double dblVal1,dblVal2,dblRep = 0;
+            bool bSaisieOpCorrect = true;
+            Console.WriteLine("Calculatrice");
+            Console.WriteLine("----------------------------");
+            // Saisie des opérands avec vérification de ces dernières
+            Console.Write("Saisire la 1ère valeur : ");
+            while(!double.TryParse(Console.ReadLine(), out dblVal1))
+            {
+                Console.Write("Saisire à nouveau la 1ère valeur : ");
+            }
+
+            Console.Write("Saisire la 2ème valeur : ");
+            while (!double.TryParse(Console.ReadLine(), out dblVal2))
+            {
+                Console.Write("Saisire à nouveau la 2ème valeur : ");
+            }
+            Console.Write("Opération à choix [+ - * / ^] :");
+            while (bSaisieOpCorrect)
+            {
+                bSaisieOpCorrect = false;
+                string strOp = Console.ReadLine();
+                switch (strOp)
+                {
+                    case "+":
+                        dblRep = dblVal1 + dblVal2;
+                        break;
+                    case "-":
+                        dblRep = dblVal1 - dblVal2;
+                        break;
+                    case "*":
+                        dblRep = dblVal1 * dblVal2;
+                        break;
+                    case "/":
+                        dblRep = dblVal1 / dblVal2;
+                        break;
+                    case "^":
+                        dblRep = Math.Pow(dblVal1, dblVal2);
+                        break;
+                    default:
+                        Console.WriteLine("Opération non reconnue par le système");
+                        Console.Write("Veuillez saisir à nouveau l'opérateur [+ - * / ^] : ");
+                        bSaisieOpCorrect = true;
+                        break;
+                }
+            }
+
+            Console.WriteLine(dblRep);
+            Console.ReadKey();
         }
     }
 }
